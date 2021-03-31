d1ea607fc018f7f9708d135013efb6cc1a367b9f
r_kuzmin <krp536@gmail.com>
2021-04-01 01:40:11 +0300

    Moving brain-games from game-engine

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,4 +5,4 @@ import prompt
 def welcome_user():
     """Input and greeting name."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}'.format(name))
+    print('Hello, {}!'.format(name))
