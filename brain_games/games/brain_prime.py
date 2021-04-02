6850f3c493f0c42b5f0c0dc1664e288c4b6d72e4
Roman <krp536@gmail.com>
2021-04-02 18:57:50 +0300

    Update

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -13,11 +13,11 @@ def main():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     number = random.randint(0, 100)
     print('Question: {}'.format(number))
-    answer = prompt.string('Your answer: ')
     if is_prime(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
+    answer = prompt.string('Your answer: ')
     if answer == correct_answer:
         print('Correct!')
         return 0
