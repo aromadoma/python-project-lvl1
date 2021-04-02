dbc8befbac738ac07b297ff75d1d20ae9eace7a7
Roman <krp536@gmail.com>
2021-04-02 17:39:17 +0300

    Delete debug prints

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,12 +5,10 @@ import random
 def main():
     first_element = random.randint(0, 100)
     hidden_elem_index = random.randint(0, 9)
-    print(hidden_elem_index)
     increment = random.randint(0, 10)
     progression = [first_element]
     for i in range(1, 10):
         progression.append(first_element + increment * i)
-    print(progression)
     print('What number is missing in the progression?')
     print('Question: ', end='')
     for element in progression:
