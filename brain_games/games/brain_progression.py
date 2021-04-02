23e478bcdf885a1a9eea32cb21a744f422560201
Roman <krp536@gmail.com>
2021-04-02 17:35:51 +0300

    Change random range for hidden_elem_index

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,11 +4,13 @@ import random
 
 def main():
     first_element = random.randint(0, 100)
-    hidden_elem_index = random.randint(0, 10)
+    hidden_elem_index = random.randint(0, 9)
+    print(hidden_elem_index)
     increment = random.randint(0, 10)
     progression = [first_element]
     for i in range(1, 10):
         progression.append(first_element + increment * i)
+    print(progression)
     print('What number is missing in the progression?')
     print('Question: ', end='')
     for element in progression:
@@ -16,8 +18,7 @@ def main():
             print(element, end=' ')
         else:
             print('..', end=' ')
-    print('\n')
-    answer = prompt.integer('Your answer: ')
+    answer = prompt.integer('\nYour answer: ')
     if answer == progression[hidden_elem_index]:
         print('Correct!')
         return 0
